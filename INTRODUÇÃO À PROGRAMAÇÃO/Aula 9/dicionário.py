f390b8e290165fc8cb452a24195eea2228896cd9
IgorValente08 <112356556+IgorValente08@users.noreply.github.com>
2022-10-14 17:14:06 -0300

    14.10.2022

---
+++ INTRODUÇÃO À PROGRAMAÇÃO/Aula 9/dicionário.py
@@ -0,0 +1,22 @@
+dicionario = {
+    1: {
+        'nome': 'joão',
+        'email': 'msm.com.br',
+        'telefone:': 11111111,
+        'qualificacao': ['Direito', 'c.contabeis']
+    },
+    2: {
+        'nome': 'João',
+        'email': 'msm.com.br',
+        'telefone:': 11111111,
+        'qualificacao': {
+            'graduacao': "Sistema para internet",
+            'pos': "Segurança da informação"
+        }
+    },
+    
+}
+
+print(dicionario[1])
+
+
---
+++ INTRODUÇÃO À PROGRAMAÇÃO/Aula 9/tuplas.py
@@ -0,0 +1,5 @@
+tupla_teste = ("endpoint", 200)
+
+print(tupla_teste[0])
+print(tupla_teste[1])
+
